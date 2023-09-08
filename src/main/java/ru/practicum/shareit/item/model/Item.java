8764c092afa4f3c97554f39e742c0273778ab677
Olga Panova <ovpanova1996@gmail.com>
2023-09-08 22:42:02 +0300

    добавлена сущность Item

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,26 @@
 package ru.practicum.shareit.item.model;
 
+import org.springframework.boot.context.properties.bind.DefaultValue;
+import ru.practicum.shareit.user.User;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 /**
  * TODO Sprint add-controllers.
  */
 public class Item {
+    long itemId;
+    @NotBlank
+    String name;
+    @Size(max = 200)
+    String description;
+    boolean available;
+    User owner;
+
+    public Item (String name, String description, boolean available) {
+        this.name = name;
+        this.description = description;
+        this.available = available;
+    }
 }
