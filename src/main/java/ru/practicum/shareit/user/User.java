0c6c048ece5d692187d097b82306a0cb9789b94f
Olga Panova <ovpanova1996@gmail.com>
2023-09-08 22:46:22 +0300

    Добавлена сущность User

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,15 @@
 package ru.practicum.shareit.user;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 /**
  * TODO Sprint add-controllers.
  */
 public class User {
+    long userId;
+    @NotBlank
+    String name;
+    @Email
+    String email;
 }
