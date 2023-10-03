8b25648f77651093ca3c23f39a7753689a9362a1
Olga Panova <ovpanova1996@gmail.com>
2023-10-03 21:37:35 +0300

    feat: добавлена логика маппера

--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -1,12 +1,21 @@
 package ru.practicum.shareit.user.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
+@Data
+@AllArgsConstructor
 public class UserDto {
     @NotBlank
     private String name;
     @Email
     @NotBlank
     private String email;
+
+//    public UserDto(String name, String email) {
+//    }
 }
--- src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
@@ -22,18 +22,32 @@ public class UserServiceImpl implements UserService {
     }
 
     public User createUser(UserDto userDto) {
+        if (userDto.getEmail() == null || userDto.getName() == null) {
+            throw new RuntimeException(); //Исправить ошибку
+        }
         increaseCount();
-        userDao.save(UserMapper.mapToExistingUser(userDto, count));
+        userDao.save(UserMapper.mapToUser(userDto, count));
         return userDao.getUserById(count); //код
      //   userDao.save(UserMapper.mapToNewUser(userDto)); //если у поля id будет аннотация generatedValue
     }
 
     public User updateUser(UserDto userDto, long userId) {
-        userDao.update(UserMapper.mapToExistingUser(userDto, userId));
+        //Здесь логика по проверке того, что пришло в DTO? Имя и email, только имя или только почта. Или в маппере?
+        //Здесь проверяем наличие email у другого юзера?
+        //делать ли try-catch, чтобы проверить, что юзер с таким id есть?
+        User user = userDao.getUserById(userId);
+        if (userDto.getName() != null) {
+            user.setName(userDto.getName());
+        } else if (userDto.getEmail() != null) {
+            user.setEmail(userDto.getEmail());
+        }
+        userDao.update(user);
+        //userDao.update(UserMapper.mapToExistingUser(userDto, userId));
         return userDao.getUserById(userId); //код
     }
 
     public UserDto getUserById(long userId) {
+        //надо ли добавить проверку на наличие юзера с таким id в базе или выводим пустоту?
         User user = userDao.getUserById(userId);
         return UserMapper.mapToUserDto(user);
     }
@@ -44,6 +58,7 @@ public class UserServiceImpl implements UserService {
     }
 
     public void deleteUser(long userId) {
+        //нужна проверка на наличие такого id?
         userDao.delete(userId);
     }
 
