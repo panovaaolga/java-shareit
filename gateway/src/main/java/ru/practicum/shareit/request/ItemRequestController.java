8769c46e67ed78b90ec3d36bfe77ed426c2a76b2
Olga Panova <ovpanova1996@gmail.com>
2023-12-20 13:23:46 +0300

    fix: исправлены методы пакета request в gateway

--- gateway/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ gateway/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -2,45 +2,46 @@ package ru.practicum.shareit.request;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import ru.practicum.shareit.request.dto.ItemRequestDto;
 import ru.practicum.shareit.request.dto.ItemRequestDtoInput;
-import ru.practicum.shareit.request.service.ItemRequestService;
 
-import java.util.List;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
 
-/**
- * TODO Sprint add-item-requests.
- */
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping(path = "/requests")
 public class ItemRequestController {
-    private final ItemRequestService requestService;
+    private final RequestClient requestClient;
 
     @PostMapping
-    public ItemRequestDto createRequest(@RequestHeader("X-Sharer-User-Id") long userId,
-                                        @Validated @RequestBody ItemRequestDtoInput dtoInput) {
-        return requestService.createRequest(userId, dtoInput);
+    public ResponseEntity<Object> createRequest(@RequestHeader("X-Sharer-User-Id") long userId,
+                                                @Validated @RequestBody ItemRequestDtoInput dtoInput) {
+        log.info("Create request {} by user with id {}", dtoInput, userId);
+        return requestClient.createRequest(userId, dtoInput);
     }
 
     @GetMapping
-    public List<ItemRequestDto> getAllByUser(@RequestHeader("X-Sharer-User-Id") long userId) {
-        return requestService.getRequestsByOwner(userId);
+    public ResponseEntity<Object> getAllByUser(@RequestHeader("X-Sharer-User-Id") long userId) {
+        log.info("Get requests of user with id {}", userId);
+        return requestClient.getAllByUser(userId);
     }
 
     @GetMapping("/all")
-    public List<ItemRequestDto> getAllRequests(@RequestHeader("X-Sharer-User-Id") long userId,
-                                               @RequestParam(defaultValue = "0") int from,
-                                               @RequestParam(defaultValue = "10") int size) {
-        return requestService.getAllRequests(userId, from, size);
+    public ResponseEntity<Object> getAllRequests(@RequestHeader("X-Sharer-User-Id") long userId,
+                                               @PositiveOrZero @RequestParam(defaultValue = "0") int from,
+                                               @Positive @RequestParam(defaultValue = "10") int size) {
+        log.info("Get all requests by user with id {}", userId);
+        return requestClient.getAllRequests(userId, from, size);
     }
 
     @GetMapping("/{requestId}")
-    public ItemRequestDto getRequestById(@RequestHeader("X-Sharer-User-Id") long userId,
+    public ResponseEntity<Object> getRequestById(@RequestHeader("X-Sharer-User-Id") long userId,
                                          @PathVariable long requestId) {
-        return requestService.getRequestById(userId, requestId);
+        log.info("Get request with id {} by user with id {}", requestId, userId);
+        return requestClient.getRequestById(userId, requestId);
     }
 }
--- gateway/src/main/java/ru/practicum/shareit/request/RequestClient.java
+++ gateway/src/main/java/ru/practicum/shareit/request/RequestClient.java
@@ -1,4 +1,45 @@
 package ru.practicum.shareit.request;
 
-public class RequestClient {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import ru.practicum.shareit.client.BaseClient;
+import ru.practicum.shareit.request.dto.ItemRequestDtoInput;
+
+import java.util.Map;
+
+public class RequestClient extends BaseClient {
+    private static final String API_PREFIX = "/requests";
+
+    @Autowired
+    public RequestClient(@Value("${shareit-server.url}") String serverUrl, RestTemplateBuilder builder) {
+        super(
+                builder
+                        .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + API_PREFIX))
+                        .requestFactory(HttpComponentsClientHttpRequestFactory::new)
+                        .build()
+        );
+    }
+
+    public ResponseEntity<Object> createRequest(long userId, ItemRequestDtoInput dtoInput) {
+        return post("", userId, dtoInput);
+    }
+
+    public ResponseEntity<Object> getAllByUser(long userId) {
+        return get("", userId);
+    }
+
+    public ResponseEntity<Object> getAllRequests(long userId, int from, int size) {
+        Map<String, Object> parameters = Map.of(
+                "from", from,
+                "size", size);
+        return get("/all?from={from}&size={size}", userId, parameters);
+    }
+
+    public ResponseEntity<Object> getRequestById(long userId, long requestId) {
+        return get("/" + requestId, userId);
+    }
 }
